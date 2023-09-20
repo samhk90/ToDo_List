21626288bd17e33aed9928a8bd9f0f05180fd127
samhk90 <mdsami9022@gmail.com>
2023-09-20 23:40:59 +0530

    added some css

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 const task=document.getElementById("task");
 const listContainer=document.getElementById("list-container");
 
-function savedata(){
 
-    localStorage.setItem('data',listContainer.innerHTML);
-
-}
 function addtodo(){
     if(task.value==''){
         alert('Plzz enter task...');
@@ -13,26 +9,38 @@ function addtodo(){
         let li=document.createElement('li');
         li.innerHTML=task.value;
         listContainer.appendChild(li);
-        let span=document.createElement("span");
-        span.innerHTML='samee';
+        let span = document.createElement("span");
+        span.innerHTML = '<i class="fas fa-times"></i>';
+        span.classList.add('delete-button'); // Add the delete-button class
         li.appendChild(span);
+        
 
     }
     task.value='';
-    savedata();
+    // savedata();
 }
-listContainer.addEventListener("click",function(e){
-    if(e.target.tagname=='li'){
+listContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === 'LI') {
         e.target.classList.toggle('checked');
         savedata();
+    } else if (e.target.tagName === 'SPAN') {
+        // Check if the clicked SPAN has a specific class or data attribute
+        if (e.target.classList.contains('delete-button')) {
+            let lis = e.target.closest('li');
+            if (lis) {
+                lis.remove();
+                savedata();
+            }
+        }
     }
-    else if(e.target.tagname=="span"){
-        e.target.parentElament.remove();
-        savedata();
-    }
-},false);
+}, false);
+
+
+ function savedata(){
+    localStorage.setItem("data",listContainer.innerHTML);
+ }
 
 function showtask(){
     listContainer.innerHTML=localStorage.getItem('data');
 }
-showtask();
+// showtask();
